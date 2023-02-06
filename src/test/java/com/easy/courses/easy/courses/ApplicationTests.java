2cee44fe416fba180aef1e87d850139087a3d756
filip.birsa@ebcont.com <birsafilip@gmail.com>
2023-02-06 15:17:37 +0100

    init commit

---
+++ src/test/java/com/easy/courses/easy/courses/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.courses.easy.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
